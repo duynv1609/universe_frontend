0b234dc5427b608e24b981d6849cd032f2d550f4
Mai Do <maidtp@dolphinsolutions.vn>
2022-12-10 11:57:14 +0700

    feat: cu-3y98hjb - convert js file

--- original/js/slider-radio.js
+++ original/js/slider-radio.js
@@ -1,106 +1,100 @@
-window.addEventListener('load', function () {
-  const sliderRadios = document.getElementsByClassName('slider-radio')
-  const animationDuration = 0.2
-  let timeout = null
+window.addEventListener('load', function() {
 
-  for (let i = 0; i < sliderRadios.length; i++) {
-    const sliderRadio = sliderRadios[i]
+	const sliderRadios = document.getElementsByClassName("slider-radio");
+	const animationDuration = 0.2;
+	let timeout = null;
 
-    const slidingPiece = document.createElement('div')
-    slidingPiece.className = 'sliding-piece'
-    sliderRadio.appendChild(slidingPiece)
-    slidingPiece.style.position = 'absolute'
-    slidingPiece.style.transition =
-      'width ' +
-      animationDuration +
-      's, height ' +
-      animationDuration +
-      's, top ' +
-      animationDuration +
-      's, left ' +
-      animationDuration +
-      's'
+	for (let i = 0; i < sliderRadios.length; i++) {
+		let sliderRadio = sliderRadios[i];
 
-    sliderRadio.addEventListener('click', handleLabelClick)
+		const slidingPiece = document.createElement('div');
+		slidingPiece.className = "sliding-piece";
+		sliderRadio.appendChild(slidingPiece);
+		slidingPiece.style.position = "absolute";
+		slidingPiece.style.transition = "width " + animationDuration + "s, height " + animationDuration + "s, top " + animationDuration + "s, left " + animationDuration + "s";
 
-    // Simply setting up the `sliderPiece` by clicking any pre-checked value;
-    const radios = sliderRadio.querySelectorAll("input[type='radio']")
-    for (let i = 0; i < radios.length; i++) {
-      if (radios[i].checked) radios[i].click()
-    }
-  }
+		sliderRadio.addEventListener('click', handleLabelClick);
 
-  function handleLabelClick(e) {
-    let label
-    let radio
-    let slidingPiece
+		// Simply setting up the `sliderPiece` by clicking any pre-checked value;
+		const radios = sliderRadio.querySelectorAll("input[type='radio']");
+		for(let i = 0; i < radios.length; i++) {if(radios[i].checked) radios[i].click() }
+	}
 
-    if (e.target.nodeName === 'INPUT') {
-      radio = e.target
-      label = radio.nextSibling
-      slidingPiece = radio.parentNode.querySelector('.sliding-piece')
+	function handleLabelClick(e) {
+		let label;
+		let radio;
+		let slidingPiece;
 
-      if (radio.checked) {
-        const labelCoords = getElementCoords(label)
+		if (e.target.nodeName === "INPUT") {
 
-        // Read function description below.
-        runUglyAnimationSetups(slidingPiece)
+			radio = e.target;
+			label = radio.nextSibling;
+			slidingPiece = radio.parentNode.querySelector(".sliding-piece");
 
-        animateSlidingPiece(labelCoords)
+			if(radio.checked) {
+				const labelCoords = getElementCoords(label);
 
-        if (timeout) {
-          clearTimeout(timeout)
-        }
-        timeout = setTimeout(function () {
-          clearUglyAnimationSetups()
+				// Read function description below.
+				runUglyAnimationSetups(slidingPiece, );
 
-          timeout = null
-        }, (animationDuration + 0.3) * 1000)
-      }
-    }
+				animateSlidingPiece(labelCoords);
 
-    function updateSlidingPieceCoords(label) {
-      animateSlidingPiece(getElementCoords(label))
-    }
+				if (timeout) {
+					clearTimeout(timeout);
+				}
+				timeout = setTimeout(function() {
+					clearUglyAnimationSetups();
 
-    function animateSlidingPiece(coords) {
-      slidingPiece.style.left = coords.x + 'px'
-      slidingPiece.style.top = coords.y + 'px'
-      slidingPiece.style.width = coords.width + 'px'
-      slidingPiece.style.height = coords.height + 'px'
-    }
+					timeout = null;
+				}, (animationDuration+0.3) * 1000);
 
-    /**
-     * This function tries to hide the checked label until the animation
-     * is over, because, in truth, the label is checked instantly. So
-     * it gets all the styles from the css. But things like border or background-color
-     * we don't want to show up instantly. So in this function we hide those styles.
-     */
-    function runUglyAnimationSetups() {
-      slidingPiece.style['transition-duration'] = animationDuration
-      slidingPiece.style.opacity = 1
-      label.style.backgroundColor = 'transparent'
-      label.style.boxShadow = 'none'
-      label.style.borderColor = 'transparent'
-    }
+			}
 
-    function clearUglyAnimationSetups() {
-      label.style.backgroundColor = ''
-      slidingPiece.style.opacity = '0'
-      label.style.boxShadow = ''
-      label.style.borderColor = ''
-    }
-  }
+		}
 
-  function getElementCoords(el) {
-    const elementPosition = { x: el.offsetLeft, y: el.offsetTop }
-    const elementSize = el.getBoundingClientRect()
+		function updateSlidingPieceCoords(label) {
+			animateSlidingPiece(getElementCoords(label));
+		}
 
-    return {
-      x: elementPosition.x,
-      y: elementPosition.y,
-      width: elementSize.width,
-      height: elementSize.height,
-    }
-  }
-})
+		function animateSlidingPiece(coords) {
+			slidingPiece.style.left = coords.x + "px";
+			slidingPiece.style.top = coords.y + "px";
+			slidingPiece.style.width = coords.width + "px";
+			slidingPiece.style.height = coords.height + "px";
+		}
+
+		/**
+		 * This function tries to hide the checked label until the animation
+		 * is over, because, in truth, the label is checked instantly. So
+		 * it gets all the styles from the css. But things like border or background-color
+		 * we don't want to show up instantly. So in this function we hide those styles.
+		 */
+		function runUglyAnimationSetups() {
+			slidingPiece.style['transition-duration'] = animationDuration;
+			slidingPiece.style.opacity = 1;
+			label.style.backgroundColor = "transparent";
+			label.style.boxShadow = "none";
+			label.style.borderColor = "transparent";
+		}
+
+		function clearUglyAnimationSetups() {
+			label.style.backgroundColor = "";
+			slidingPiece.style.opacity = '0';
+			label.style.boxShadow = "";
+			label.style.borderColor = "";
+		}
+
+	}
+
+	function getElementCoords(el) {
+		const elementPosition =  {x: el.offsetLeft, y: el.offsetTop};
+		const elementSize = el.getBoundingClientRect();
+
+		return {
+			x: elementPosition.x,
+			y: elementPosition.y,
+			width: elementSize.width,
+			height: elementSize.height
+		}
+	}
+});
